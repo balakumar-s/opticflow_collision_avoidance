74bfeff9c469b6e1392b39af9fbe3d2ae5dd337e
balakumar-s <s.balakumar@outlook.com>
2015-04-25 17:38:11 -0600

    commented code and wrote TODO

--- src/optic_flow_LK.cpp
+++ src/optic_flow_LK.cpp
@@ -9,8 +9,10 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <std_msgs/Float32.h>
 
 void imageCallback(const sensor_msgs::ImageConstPtr&);
+ros::Publisher left_pub,right_pub;
 using namespace cv;
 int main(int argc,char** argv)
 {
@@ -20,6 +22,8 @@ int main(int argc,char** argv)
   cv::namedWindow("left");
   cv::namedWindow("right");
 	ros::Subscriber image_sub=n.subscribe("/usb_cam_node/image_raw",1,imageCallback);
+  left_pub=n.advertise<std_msgs::Float32>("optic/left",1);
+  right_pub=n.advertise<std_msgs::Float32>("optic/right",1);
 	ros::spin();
 	return 0;
 }
@@ -36,13 +40,16 @@ void imageCallback(const sensor_msgs::ImageConstPtr& im_msg)
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
+    //Image is converted to grayscale
     Mat input_image=cv_ptr->image;
     Mat gray_image_1;
     cvtColor(input_image,gray_image_1,CV_BGR2GRAY);
     Mat gray_image=Mat(gray_image_1.rows,gray_image_1.cols,CV_8U);
     gray_image=gray_image_1;
+    // Grayscale image is displayed
     cv::imshow("grayscale_input",gray_image);
     cv::waitKey(1);
+    // Image is split into left and right
     Mat left_image=Mat(720,1280/2,CV_8U);
     Mat right_image=Mat(720,1280/2,CV_8U);
   for (int i=0;i<gray_image.rows;i++)
@@ -59,11 +66,20 @@ void imageCallback(const sensor_msgs::ImageConstPtr& im_msg)
       }
     }
    } 
+   // Left and right images are streamed
    imshow("left",left_image);
        cv::waitKey(1);
-
    imshow("right",right_image);
        cv::waitKey(1);
+  // TODO: Implement optic flow method
 
+  // Find maximum limit of vectors
+
+  // Write float values
+    std_msgs::Float32 left_,right_;
+    left_.data=1;
+    right_.data=1;
+    left_pub.publish(left_);
+    right_pub.publish(right_);
 
 }
