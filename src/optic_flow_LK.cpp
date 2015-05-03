d1476d48590777c5ea68c6432c1b6c4a1bce2567
balakumar-s <s.balakumar@outlook.com>
2015-05-03 12:23:56 -0600

    sped up code

--- src/optic_flow_LK.cpp
+++ src/optic_flow_LK.cpp
@@ -19,11 +19,9 @@ void imageCallback(const sensor_msgs::ImageConstPtr&);
 ros::Publisher left_pub,right_pub;
 using namespace cv;
 using namespace Eigen;
-int n=3;
+int n=1;
 int scale=5;
-//std::vector<Point2f> optic_lucas(Mat,Mat);
-//Point2f(float,float) optic_lucas(Mat,Mat);
-MatrixXf optic_lucas(Mat,Mat);
+void optic_lucas(Mat,Mat);
 float square(float);
 
 int count=1;
@@ -100,46 +98,13 @@ void imageCallback(const sensor_msgs::ImageConstPtr& im_msg)
     cv::Scalar line_color=CV_RGB(64, 64, 255);
     Mat optic_image;
     cvtColor(left_image,optic_image, CV_GRAY2RGB);
-    MatrixXf left_flow;
-    left_flow=optic_lucas(left_image,left_image);
- /* for(int i=n;i<left_image.rows-n;i++)
-  {
-    for(int j=n;j<left_image.cols-n;j++)
-    {
-      CvPoint p,q;
-      p.x=i;
-      p.y=j;
-      q.x=left_flow(i,j);
-      q.y=left_flow(i+left_image.rows-1,j+left_image.cols-1);
-      double angle;   
-      angle = atan2( (double) p.y - q.y, (double) p.x - q.x );
-      double hypotenuse;  hypotenuse = sqrt( square(p.y - q.y) + square(p.x - q.x));
-      //q.x = (int) (p.x - 3 * hypotenuse * cos(angle));
-    //  q.y = (int) (p.y - 3 * hypotenuse * sin(angle));
-      line( optic_image, p, q, line_color, line_thickness, CV_AA, 0 );
-      /* Now draw the tips of the arrow.  I do some scaling so that the
-        * tips look proportional to the main line of the arrow.
-        */   
-     /* p.x = (int) (q.x + 9 * cos(angle + PI / 4));
-      p.y = (int) (q.y + 9 * sin(angle + PI / 4));    
-      line( optic_image, p, q, line_color, line_thickness, CV_AA, 0 );
-      p.x = (int) (q.x + 9 * cos(angle - PI / 4));
-      p.y = (int) (q.y + 9 * sin(angle - PI / 4));    
-      line( optic_image, p, q, line_color, line_thickness, CV_AA, 0 );
-      */
-
-  /*  }
-  }
-  
-  
-  imshow("left_optic_flow",optic_image);
-  cv::waitKey(1);
-  */
+    MatrixXd left_flow;
+    optic_lucas(left_image,left_image);
   }
   left_image_prev=left_image;
   count++;
   ROS_INFO("%d",count);
- // Point2f right_vec=optic_lucas(right_image_prev,right_image);
+ 
   //Plot vectors:
 
   // Find maximum limit of vectors
@@ -153,40 +118,14 @@ void imageCallback(const sensor_msgs::ImageConstPtr& im_msg)
 
 }
 
-//std::vector<Point2f> optic_lucas(Mat first_image,Mat second_image)
-//const Point2f&(float,float) optic_lucas(Mat first_image,Mat second_image)
-MatrixXf optic_lucas(Mat first_image_in,Mat second_image_in)
+void optic_lucas(Mat first_image_in,Mat second_image_in)
 {
   Mat first_image;
   Mat second_image;
   GaussianBlur(first_image_in,first_image,Size(n,n),0,0);
   GaussianBlur(second_image_in,second_image,Size(n,n),0,0);
-  Mat x_derivative=Mat(first_image.rows,first_image.cols,CV_8U);
-  Mat y_derivative=Mat(first_image.rows,first_image.cols,CV_8U);
   Mat time_derivative=Mat(first_image.rows,first_image.cols,CV_8U);
-  for(int i=0;i<first_image.rows;i++)
-  {
-    for(int j=0;j<first_image.cols;j++)
-    {
-      if(i>0&&i<first_image.rows-1)
-      {
-        x_derivative.at<uchar>(i,j)=(first_image.at<uchar>(i-1,j)-first_image.at<uchar>(i+1,j))/2;
-      }
-      if(j>0&&j<first_image.cols-1)
-      {
-        y_derivative.at<uchar>(i,j)=(first_image.at<uchar>(i,j-1)-first_image.at<uchar>(i,j+1))/2;
-      }
-    }
-  }
   time_derivative=second_image-first_image;
- /* imshow("x_derivative",x_derivative);
-  cv::waitKey(1);
-  imshow("y_derivative",y_derivative);
-  cv::waitKey(1);
-  imshow("time_derivative",time_derivative);
-  cv::waitKey(1);*/
-  
-  MatrixXf optic_flow_matrix(first_image.rows*2,first_image.cols*2);
   
   Mat optic_image;
   cvtColor(first_image,optic_image, CV_GRAY2RGB);
@@ -197,8 +136,8 @@ MatrixXf optic_lucas(Mat first_image_in,Mat second_image_in)
     for(int j=n;j<first_image.cols-n;j++)
     {
 
-      MatrixXf A(2,n*n);
-      MatrixXf B(1,n*n);
+      MatrixXd A(2,n*n);
+      MatrixXd B(1,n*n);
       int temp_counter=0;
       for(int k=0;k<n;k++)
       {
@@ -206,30 +145,28 @@ MatrixXf optic_lucas(Mat first_image_in,Mat second_image_in)
         for(int l=0;l<n;l++)
         {
           int col_marker=l-1;
-          A(0,temp_counter)=x_derivative.at<uchar>(i+row_marker,j+col_marker);
-          A(1,temp_counter)=y_derivative.at<uchar>(i+row_marker,j+col_marker);
-          B(0,temp_counter)=time_derivative.at<uchar>(i+row_marker,j+col_marker);
+          float x_d=(first_image.at<uchar>(i-1+row_marker,j+col_marker)-first_image.at<uchar>(i+1+row_marker,j+col_marker))/2;
+          float y_d=(first_image.at<uchar>(i+row_marker,j-1+col_marker)-first_image.at<uchar>(i+row_marker,j+1+col_marker))/2;
+
+          A(0,temp_counter)=x_d;
+          A(1,temp_counter)=y_d;
+          B(temp_counter,0)=time_derivative.at<uchar>(i+row_marker,j+col_marker);
           temp_counter++;
         }
       }
-      MatrixXf A_transpose=A.transpose();
-      MatrixXf B_transpose=B.transpose();
-      MatrixXf temp_toinverse=(A*A_transpose);
-      MatrixXf temp_vector= ((temp_toinverse.inverse())*A)*B_transpose;
+      MatrixXd A_transpose=A.transpose();
+      MatrixXd temp_toinverse=(A*A_transpose);
+      MatrixXd temp_vector=((temp_toinverse.inverse())*A)*B;
       
-      MatrixXf flow_matrix=-temp_vector;
-      optic_flow_matrix(i,j)=flow_matrix(0,0);
-      optic_flow_matrix(i+first_image.rows-1,j+first_image.cols-1)=flow_matrix(1,0);
-      
-      MatrixXf left_flow=optic_flow_matrix;
+      MatrixXd flow_matrix=-temp_vector;
       CvPoint p,q;
       p.x=i;
       p.y=j;
-      q.x=left_flow(i,j);
-      q.y=left_flow(i+first_image.rows-1,j+first_image.cols-1);
-      double angle;   
-      angle = atan2( (double) p.y - q.y, (double) p.x - q.x );
-      double hypotenuse;  hypotenuse = sqrt( square(p.y - q.y) + square(p.x - q.x));
+      q.x=flow_matrix(0,0);
+      q.y=flow_matrix(1,0);
+     // double angle;   
+     // angle = atan2( (double) p.y - q.y, (double) p.x - q.x );
+     // double hypotenuse;  hypotenuse = sqrt( square(p.y - q.y) + square(p.x - q.x));
       //q.x = (int) (p.x - 3 * hypotenuse * cos(angle));
     //  q.y = (int) (p.y - 3 * hypotenuse * sin(angle));
       line( optic_image, p, q, line_color, line_thickness, CV_AA, 0 );    
@@ -241,7 +178,7 @@ MatrixXf optic_lucas(Mat first_image_in,Mat second_image_in)
   cv::waitKey(1);
 
 
-  return optic_flow_matrix;
+  //return optic_flow_matrix;
 }
 float square(float x)
 {
